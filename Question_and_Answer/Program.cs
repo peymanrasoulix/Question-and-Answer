9227bd5f71365ed22fc4883a72d222fa46011efb
Peyman <61409421+peymanrasoulix@users.noreply.github.com>
2023-03-10 18:17:13 +0330

    Upload simple PWA project on Github

--- Question_and_Answer/Program.cs
+++ Question_and_Answer/Program.cs
@@ -1,8 +1,8 @@
-﻿//Peyman Rasouli -- Q&A
-string digit;
+﻿// Peyman Rasouli -- Q&A
+
 
 // data haei ke masalan az server gereftim
-List<string> qu = new List<string>()
+List<string> questions = new List<string>()
 {   "are you a human??",
     "Are you a man?",
     "Are you young?",
@@ -13,11 +13,12 @@ List<string> qu = new List<string>()
     "Are you satisfied with your life?",
     "Did you like this program?"
 };
-List<string> an = new List<string>();
+List<string> Answers = new List<string>();
+string digit;
 
 start_questions();
 
-//vorod meghdare yes ya no
+// vorod meghdare yes ya no
 void locked_ys(string q)
 {
     Console.WriteLine(q);
@@ -27,32 +28,32 @@ void locked_ys(string q)
     } while (digit.ToLower() != "y" && digit.ToLower() != "n" && digit.ToLower() != "غ" && digit.ToLower() != "د");
     digit = digit.ToLower();
 }
-//bakhshe ejraei barname
+// bakhshe ejraei barname
 void start_questions()
 {
-    foreach (var qua in qu)
+    foreach (var question in questions)
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        locked_ys(qua);
-        Switch_questions(qua, digit);
+        locked_ys(question);
+        Switch_questions(question, digit);
         Console.Clear();
     }
     Console.Clear();
     Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("You are a {0} {1} {2} {3} children\nand your monthly income {4}\n{5}", an[1], an[2], an[0], an[4], an[5], an[6]);
+    Console.WriteLine("You are a {0} {1} {2} {3} children\nand your monthly income {4}\n{5}", Answers[1], Answers[2], Answers[0], Answers[4], Answers[5], Answers[6]);
     Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine(an[7]);
+    Console.WriteLine(Answers[7]);
     Console.WriteLine();
     Console.ForegroundColor = ConsoleColor.White;
     locked_ys("Do you want to continue?");
     if (digit == "y")
     {
         Console.Clear();
-        an.Clear();
+        Answers.Clear();
         start_questions();
     }
 }
-//dastor swidch baraye tashkhis soval ha
+// dastor swidch baraye tashkhis soval ha
 void Switch_questions(string a,string d)
 {
     if (digit == "y" || digit == "غ")
@@ -60,28 +61,28 @@ void Switch_questions(string a,string d)
         switch (a)
         {
             case "Are you a man?":
-                an.Add("Man");
+                Answers.Add("Man");
                 break;
             case "Are you young?":
-                an.Add("Young");
+                Answers.Add("Young");
                 break;
             case "Are you married?":
-                an.Add("Married");
+                Answers.Add("Married");
                 break;
             case "Have you ever married?":
-                an.Add("Yes");
+                Answers.Add("Yes");
                 break;
             case "Do you have children?":
-                an.Add("Without");
+                Answers.Add("Without");
                 break;
             case "Is your monthly income over 15 tomans?":
-                an.Add("Is over 15 tomans");
+                Answers.Add("Is over 15 tomans");
                 break;
             case "Are you satisfied with your life?":
-                an.Add("So you are satisfied with your life");
+                Answers.Add("So you are satisfied with your life");
                 break;
             case "Did you like this program?":
-                an.Add("Thank you for choosing us :)");
+                Answers.Add("Thank you for choosing us :)");
                 break;
 
         }
@@ -90,28 +91,28 @@ void Switch_questions(string a,string d)
         switch (a)
         {
             case "Are you a man?":
-                an.Add("Woman");
+                Answers.Add("Woman");
                 break;
             case "Are you young?":
-                an.Add("Old");
+                Answers.Add("Old");
                 break;
             case "Are you married?":
-                an.Add("Single");
+                Answers.Add("Single");
                 break;
             case "Have you ever married?":
-                an.Add("No");
+                Answers.Add("No");
                 break;
             case "Do you have children?":
-                an.Add("With");
+                Answers.Add("With");
                 break;
             case "Is your monthly income over 15 tomans?":
-                an.Add("Is below 15 tomans");
+                Answers.Add("Is below 15 tomans");
                 break;
             case "Are you satisfied with your life?":
-                an.Add("Then you are not satisfied with your life");
+                Answers.Add("Then you are not satisfied with your life");
                 break;
             case "Did you like this program?":
-                an.Add("We hope to be able to satisfy you next time");
+                Answers.Add("We hope to be able to satisfy you next time");
                 break;
             case "are you a human??":
                 Console.ReadKey();
